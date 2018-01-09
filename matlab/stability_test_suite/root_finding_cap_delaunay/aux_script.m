a837b0c4d712a72979a6ef9644aaaed390f5e97d
Lukasz G <lukegrzym@gmail.com>
2018-01-09 19:50:51 +0100

    a lot of refactoring

--- matlab/stability_test_suite/root_finding_cap_delaunay/aux_script.m
+++ matlab/stability_test_suite/root_finding_cap_delaunay/aux_script.m
@@ -1,11 +1,13 @@
 close all
-range = [-1.1 1.1];
+% range = [-1.1 1.1];
+% 
+% x = 0:0.05:2*pi;
+% plot(sin(x), cos(x));
+% xlim(range)
+% ylim(range)
+% 
+% hold on
 
-x = 0:0.01:2*pi;
-plot(sin(x), cos(x));
-xlim(range)
-ylim(range)
-
-hold on
-
-V = 
+points = [0 0; 5 1];
+sqrt(sum((points(1,:) - points(2,:).^ 2)));
+new_vertice = [(edge_start(1, 1)+edge_end(1,1))/2 (edge_start(1, 2)+edge_end(1,2))/2];
